8959c34406a2cc15f97525682b4468b1d6798917
lynnsey <briml@seattleu.edu>
2018-06-04 02:35:18 -0700

    hw files

---
+++ empdata.js
@@ -0,0 +1,173 @@
+{
+  "data": [
+    {
+      "name": "Pierre-Emerick Aubameyang",
+      "nationality": "Gabon",
+      "position": "Striker",
+      "club_team": "Borussia Dortmund",
+      "age": "28"
+    },
+    {
+      "name": "Leonardo Bonucci",
+      "nationality": "Italian",
+      "position": "Defender",
+      "club_team": "AC Milan",
+      "age": "30"
+    },
+    {
+      "name": "Gianluigi Buffon",
+      "nationality": "Italian",
+      "position": "Goalkeeper",
+      "club_team": "Juventus",
+      "age": "40"
+    },
+    {
+      "name": "Dani Carvajal",
+      "nationality": "Spanish",
+      "position": "Defender",
+      "club_team": "Real Madrid",
+      "age": "26"
+    },
+    {
+      "name": "Cristiano Ronaldo",
+      "nationality": "Portuguese",
+      "position": "Forward",
+      "club_team": "Real Madrid",
+      "age": "33"
+    },
+    {
+      "name": "Paulo Dybala",
+      "nationality": "Argentine",
+      "position": "Forward",
+      "club_team": "Juventus",
+      "age": "24"
+    },
+    {
+      "name": "Antoine Griezmann",
+      "nationality": "French",
+      "position": "Striker",
+      "club_team": "Atlético Madrid",
+      "age": "27"
+    },
+    {
+      "name": "Eden Hazard",
+      "nationality": "Belgian",
+      "position": "Forward",
+      "club_team": "Chelsea",
+      "age": "27"
+    },
+    {
+      "name": "Zlatan Ibrahimović",
+      "nationality": "Swedish",
+      "position": "Forward",
+      "club_team": "LA Galaxy",
+      "age": "36"
+    },
+    {
+      "name": "Andrés Iniesta",
+      "nationality": "Spanish",
+      "position": "Midfielder",
+      "club_team": "FC Barcelona",
+      "age": "33"
+    },
+    {
+      "name": "Harry Kane",
+      "nationality": "English",
+      "position": "Striker",
+      "club_team": "Tottenham Hotspur",
+      "age": "24"
+    },
+    {
+      "name": "N'Golo Kanté",
+      "nationality": "French",
+      "position": "Midfielder",
+      "club_team": "Chelsea",
+      "age": "27"
+    },
+    {
+      "name": "Toni Kroos",
+      "nationality": "German",
+      "position": "Midfielder",
+      "club_team": "Real Madrid",
+      "age": "28"
+    },
+    {
+      "name": "Robert Lewandowski",
+      "nationality": "Polish",
+      "position": "Striker",
+      "club_team": "Bayern Munich",
+      "age": "29"
+    },
+    {
+      "name": "Marcelo",
+      "nationality": "Brazilian",
+      "position": "Defender",
+      "club_team": "Real Madrid",
+      "age": "29"
+    },
+    {
+      "name": "Lionel Messi",
+      "nationality": "Argentine",
+      "position": "Forward",
+      "club_team": "FC Barcelona",
+      "age": "31"
+    },
+    {
+      "name": "Luka Modrić",
+      "nationality": "Croatian",
+      "position": "Midfielder",
+      "club_team": "Real Madrid",
+      "age": "32"
+    },
+    {
+      "name": "Keylor Navas",
+      "nationality": "Costa Rican",
+      "position": "Goalkeeper",
+      "club_team": "Real Madrid",
+      "age": "31"
+    },
+    {
+      "name": "Manuel Neuer",
+      "nationality": "German",
+      "position": "Goalkeeper",
+      "club_team": "Bayern Munich",
+      "age": "32"
+    },
+    {
+      "name": "Neymar",
+      "nationality": "Brazilian",
+      "position": "Striker",
+      "club_team": "Paris Saint-Germain",
+      "age": "26"
+    },
+    {
+      "name": "Sergio Ramos",
+      "nationality": "Spanish",
+      "position": "Defender",
+      "club_team": "Real Madrid",
+      "age": "32"
+    },
+    {
+      "name": "Alexis Sánchez",
+      "nationality": "Chilean",
+      "position": "Forward",
+      "club_team": "Arsenal",
+      "age": "29"
+    },
+    {
+      "name": "Luis Suárez",
+      "nationality": "Uruguayan",
+      "position": "Striker",
+      "club_team": "FC Barcelona",
+      "age": "31"
+    },
+    {
+      "name": "Arturo Vidal",
+      "nationality": "Chilean",
+      "position": "Midfielder",
+      "club_team": "Bayern Munich",
+      "age": "31"
+    }
+
+  ]
+}
